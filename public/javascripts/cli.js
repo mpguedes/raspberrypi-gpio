2bda879f42dbf6ff5a0bba2e9f6abfdc79d46c2f
Marcio Guedes <mpguedes@gmail.com>
2015-09-29 02:57:26 +0000

    Normal commit

---
+++ public/javascripts/cli.js
@@ -0,0 +1,19 @@
+function go() {
+   $.ajax('/go');
+}
+
+function goback() {
+   $.ajax('/back');
+}
+
+function stop() {
+   $.ajax('/stop');
+}
+
+function left() {
+   $.ajax('/left');
+}
+
+function right() {
+   $.ajax('/right');
+}
