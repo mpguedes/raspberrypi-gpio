ef6f39baa31a7bb0491a3ccbcff79f08ba7a1216
Marcio Guedes <mpguedes@gmail.com>
2015-09-29 04:27:11 +0000

    GPIO fix

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
+//GPIO
+var Gpio = require('onoff').Gpio,
+led = new Gpio(14, 'out');
+
+
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-//var cookieParser = require('cookie-parser');
 var compression = require('compression');
 var bodyParser = require('body-parser');
 
@@ -15,12 +20,9 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-//app.use(cookieParser());
 app.use(compression());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -28,6 +30,7 @@ app.use('/', routes);
 app.use('/users', users);
 
 app.get('/go', function (req, res) {
+  led.write(1, function(e){if(e != null)console.error(JSON.stringify(e))});
   res.json({go: true});
 });
 
@@ -44,8 +47,10 @@ app.get('/right', function (req, res) {
 });
 
 app.get('/stop', function (req, res) {
+  led.write(0);
   res.json({stop: true});
 });
+
 // catch 404 and forward to error handler
 
 app.use(function(req, res, next) {
